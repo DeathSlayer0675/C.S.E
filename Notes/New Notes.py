cb6cc7a3febe1c92fb66fc34595cba6636e155e0
DeathSlayer0675 <44374124+DeathSlayer0675@users.noreply.github.com>
2018-11-27 09:48:14 -0800

    Done

--- Notes/New Notes.py
+++ Notes/New Notes.py
@@ -25,3 +25,61 @@ print(disney_movies[0:3])
 print(disney_movies[0:4])
 print(disney_movies[0:])
 print(disney_movies[:4])
+
+food_list = ["pizza", "tamales", "pie", "enchiladas", "burrito",
+             "sushi", "poke", "flan", "poutine", "noodles", "chicken",
+             "chile", "hot wings", "salmon", "chips", "lasagna", "soup",
+             "fettuccine", "salad", "carne asada", "calamari", "tacos"]
+print(len(food_list))
+
+# Adding stuff to a list
+food_list.append("bacon")
+food_list.append("eggs")
+# Notice that everything is object.method(parameters)
+print(food_list)
+
+food_list.insert(1, "waffles")
+print(food_list)
+
+# Removing things from a list
+food_list.remove("salad")
+print(food_list)
+
+snacks = ["ice cream", "pudding", "chips"]
+print(len(snacks))
+
+snacks.append("cookies")
+print(snacks)
+
+snacks.remove("chips")
+print(snacks)
+
+# Also removing stuff from a list
+print(food_list)
+food_list.pop(0)
+print(food_list)
+
+# Find the index of an item
+print(food_list.index("chicken"))
+
+# Changing things into a list
+string1 = "turquoise"
+list1 = list(string1)
+print(list1)
+
+# Hangman hints
+for i in range(len(list1)):
+    if list1[i] == "u":
+        list1.pop(i)
+        list1.insert(i, "*")
+'''
+For character in list1:
+    if character == "u":
+        # repace with a *
+        current_index = list1.index(character)
+        list1.pop(current_index)
+        list1.insert(current_index, "*")
+
+
+'''
+
