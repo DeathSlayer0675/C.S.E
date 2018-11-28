2bf7fdfa2827ea9c2470be399d42e11c36bc6d81
DeathSlayer0675 <44374124+DeathSlayer0675@users.noreply.github.com>
2018-11-28 09:44:50 -0800

    Easy
    Like REALLY EASY

---
+++ Notes/Michael Alvarez - Mad Lib Part 2.py
@@ -0,0 +1,24 @@
+list1 = []
+
+list1.append(input("Type in an adjective: "))
+list1.append(input("Type in a noun: "))
+list1.append(input("Type in a thing: "))
+list1.append(input("Type in a verb: "))
+list1.append(input("Type in a place: "))
+list1.append(input("Type in a feeling: "))
+list1.append(input("Type in a noun: "))
+list1.append(input("Type in a part of the body: "))
+list1.append(input("Type in a living thing: "))
+list1.append(input("Type in an adjective: "))
+list1.append(input("Type in a vehicle: "))
+list1.append(input("Type in a noun: "))
+list1.append(input("Type in a noun: "))
+
+print("Remember when you were a %s eight-year-old %s riding a %s with your friends, %s each other around the %s?" %
+      (list1[0], list1[1], list1[2], list1[3], list1[4]))
+print("Remember that feeling of absolute %s as you felt the %s in your hair and the smile it put on your %s?" %
+      (list1[5], list1[6], list1[7]))
+print("I never thought I would feel that way as an adult until my %s presented me with a brand-new %s %s." %
+      (list1[8], list1[9], list1[10]))
+print("At first, I was a bit skeptical about the complete idea of commuting by %s." % list1[11])
+print("But one morning, a couple of %s later, I completely changed my mind." % list1[12])
