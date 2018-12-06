afce03bf63b34b2773d24a53658f62dfd4b5ec4c
DeathSlayer0675 <44374124+DeathSlayer0675@users.noreply.github.com>
2018-12-06 09:46:24 -0800

    INPUT

--- Notes/Michael Alvarez - Challenges.py
+++ Notes/Michael Alvarez - Challenges.py
@@ -8,12 +8,27 @@ name1 = challenge1("Michael", "Alvarez")
 print(name1)
 
 # Challenge 2
-def challenge2():
-    return
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+    print("{0} is Even".format(num))
+else:
+    print("{0} is Odd".format(num))
 # Challenge 3
+b = int(input("Input the base : "))
+h = int(input("Input the height : "))
 
+area = b*h/2
+
+print("area = ", area)
 # Challenge 4
 
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
 # Challenge 5
 r = 3
 area = 3.141592653589793238246*r**2
