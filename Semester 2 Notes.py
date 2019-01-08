8cbc24ea7fbbbce661a6685b4f5f32d1b160fc63
Michael Alvarez <44374124+DeathSlayer0675@users.noreply.github.com>
2019-01-08 09:44:46 -0800

    I DID IT

--- Semester 2 Notes.py
+++ Semester 2 Notes.py
@@ -1,12 +1,22 @@
 print("Hello World")
 
-# Hello beaner 
+# Hello beaner
 
+cars = 5
+driving = True
 
+print("I have %d cars" % cars)
+print("I have " + str(cars) + " cars")
 
+age = input("How old are you?")
+print("%s?!? You belong in a museum." % age)
 
+colors = ["blue", "turquoise", "pink", "orange", "black"]
+colors.append("red")
 
+print(colors)
+colors.pop(0)
+print(colors)
+print(colors[2])
 
-
-
-
+print(len(colors))
