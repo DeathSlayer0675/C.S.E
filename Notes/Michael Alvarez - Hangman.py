69aedc0c8bdbb4f23a0cf671114726d326f8b9a3
Michael Alvarez <44374124+DeathSlayer0675@users.noreply.github.com>
2019-01-16 09:44:42 -0800

    Hang me

--- Notes/Michael Alvarez - Hangman.py
+++ Notes/Michael Alvarez - Hangman.py
@@ -1,7 +1,23 @@
 import random
 word_list = ['sphinx', 'concatenation', 'pharaoh', 'liaison', 'nugget',
              'yacht', 'conscience', 'playwright', 'nightmare', 'handkerchief']
+word = random.choice(word_list)
+print(word)
+letters_guessed = []
+guesses = 8
 
-output = []
+while guesses > 0:
+    # Generate the hidden word
+    output = []
+    
+    # Ask for input
+    guess = input("Guess a letter: ")
+    letters_guessed.append(guess)
 
-print(random.choice(word_list))
+    # Check to see if the letter is in the word
+    if guess in word:
+        print("I FOUND ONE!!!!!")
+    else:
+        guesses -= 1
+    print("You have %s guesses left" % guesses)
+print("GAME OVER")
