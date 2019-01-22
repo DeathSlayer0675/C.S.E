ef8e805338ac3e022b97a8a74c8c31e4c20e7661
Michael Alvarez <44374124+DeathSlayer0675@users.noreply.github.com>
2019-01-22 09:45:13 -0800

    Bean

--- Notes/Michael Alvarez - Hangman.py
+++ Notes/Michael Alvarez - Hangman.py
@@ -1,26 +1,101 @@
 import random
+import sys
+
+
 word_list = ['sphinx', 'concatenation', 'pharaoh', 'liaison', 'nugget',
              'yacht', 'conscience', 'playwright', 'nightmare', 'handkerchief']
 word = random.choice(word_list)
-print(word)
-output = []
-length = (len(word))
-for i in range(length):
-    output.append('_')
-    print(output)
-letters_guessed = []
-guesses = 8
-while guesses > 0:
+import random
+import sys
 
-    # Ask for input
-    guess = input("Guess a letter: ")
-    letters_guessed.append(guess)
 
-# Check to see if the letter is in the word
-    if guess in word:
-        print("I FOUND ONE!!!!!")
-    else:
-        guesses -= 1
-    print("You have %s guesses left" % guesses)
+guess_word = []
+secretWord = random.choice(word_list) # lets randomize single word from the list
+length_word = len(secretWord)
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+letter_storage = []
 
-print("GAME OVER")
+
+
+def beginning():
+    print("Hello Mate!\n")
+
+    while True:
+        name = input("Please enter Your name\n").strip()
+
+        if name == '':
+            print("You can't do that! No blank lines")
+        else:
+            break
+
+beginning()
+
+
+
+def newfunc():
+    print("Well, that's perfect moment to play some Hangman!\n")
+
+    while True:
+        gameChoice = input("Would You?\n").upper()
+
+        if gameChoice == "YES" or gameChoice == "Y":
+            break
+        elif gameChoice == "NO" or gameChoice == "N":
+            sys.exit("That's a shame! Have a nice day")
+        else:
+            print("Please Answer only Yes or No")
+            continue
+
+newFunc()
+
+
+
+def change():
+
+    for character in secretWord: # printing blanks for each letter in secret word
+        guess_word.append("-")
+
+    print("Ok, so the word You need to guess has", length_word, "characters")
+
+    print("Be aware that You can enter only 1 letter from a-z\n\n")
+
+    print(guess_word)
+
+
+
+def guessing():
+    guess_taken = 1
+
+    while guess_taken < 10:
+
+
+        guess = input("Pick a letter\n").lower()
+
+        if not guess in alphabet: #checking input
+            print("Enter a letter from a-z alphabet")
+        elif guess in letter_storage: #checking if letter has been already used
+            print("You have already guessed that letter!")
+        else:
+
+            letter_storage.append(guess)
+            if guess in secretWord:
+                print("You guessed correctly!")
+                for x in range(0, length_word): #This Part I just don't get it
+                    if secretWord[x] == guess:
+                        guess_word[x] = guess
+                        print(guess_word)
+
+                if not '-' in guess_word:
+                    print("You won!")
+                    break
+            else:
+                print("The letter is not in the word. Try Again!")
+                guess_taken += 1
+                if guess_taken == 10:
+                    print(" Sorry Mate, You lost :<! The secret word was",         secretWord)
+
+
+change()
+guessing()
+
+print("Game Over!")
