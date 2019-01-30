d7fafc0deaeb6c9be22ef1bcb07d6dcfe1152445
Michael Alvarez <44374124+DeathSlayer0675@users.noreply.github.com>
2019-01-30 09:45:48 -0800

    Dictionary

---
+++ Notes/Michael Alvarez - Dictionary World Map.py
@@ -0,0 +1,24 @@
+world_map = {
+    "R19A": {
+        'NAME': "",
+        'DESCRIPTION': "",
+        'PATHS': {
+            'NORTH':"PARKING_LOT"
+        }
+    },
+    'PARKING_LOT': {
+    'NAME'
+    'DESCRIPTION': "This is the classroom you are in right now.",
+    'PATHS': {
+        "NORTH": "PARKING_LOT"
+    }
+},
+'PARKING_LOT': {
+    'NAME': "The North Parking Lot"
+    'Description': "There are a couple cars parked here.",
+    'PATHS': {
+        'SOUTH': 'R19A',
+
+        }
+    }
+}
