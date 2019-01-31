7cb6322535e1bc13ed6955da256b67108cc4e051
Michael Alvarez <44374124+DeathSlayer0675@users.noreply.github.com>
2019-01-31 09:46:07 -0800

    The Journey Begins

--- Notes/Michael Alvarez - Dictionary World Map.py
+++ Notes/Michael Alvarez - Dictionary World Map.py
@@ -1,24 +1,46 @@
 world_map = {
     "R19A": {
-        'NAME': "",
-        'DESCRIPTION': "",
+        'NAME': "Mr. Wiebe's Room",
+        'DESCRIPTION': "This is the classroom you are in right now."
+                       "There are two doors on the north wall.",
         'PATHS': {
-            'NORTH':"PARKING_LOT"
+            'NORTH': "PARKING_LOT"
         }
     },
     'PARKING_LOT': {
-    'NAME'
-    'DESCRIPTION': "This is the classroom you are in right now.",
-    'PATHS': {
-        "NORTH": "PARKING_LOT"
-    }
-},
-'PARKING_LOT': {
-    'NAME': "The North Parking Lot"
-    'Description': "There are a couple cars parked here.",
-    'PATHS': {
-        'SOUTH': 'R19A',
+        'NAME'
+        'DESCRIPTION': "This is the classroom you are in right now.",
+        'PATHS': {
+            "NORTH": "PARKING_LOT"
+
+        }
+    },
+    'PARKING LOT': {
+        'NAME': "The North Parking Lot",
+        'Description': "There are a couple cars parked here.",
+        'PATHS': {
+            'SOUTH': 'R19A'
 
         }
     }
 }
+
+playing = True
+current_node = world_map['R19A']
+directions = ['NORTH', 'EAST', 'SOUTH', 'WEST', 'UP', 'DOWN']
+
+while playing:
+    print(current_node['NAME'])
+    print(current_node['DESCRIPTION'])
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command.upper() in directions:
+        try:
+            room_name = current_node['PATHS'][command.upper()]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way")
+    else:
+        print("Command Not Found")
+
