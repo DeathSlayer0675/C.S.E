e6cf488f2c1d1034b1f0d89e8f4350ac403f36f1
Michael Alvarez <44374124+DeathSlayer0675@users.noreply.github.com>
2019-02-14 09:44:35 -0800

    FRUIT SALAD YUMMY YUMMY

---
+++ Notes/Michael Alvarez - Object.py
@@ -0,0 +1,13 @@
+class Computer(object):
+    def __init__(self, charge_left=90, broke=True):
+        self.screen = True
+        self.camera = 1
+        self.microphone = 1
+        self.battery_left = charge_left
+        self.pod = broke
+    def charge(self, time):
+        self.battery_left += time
+        if self.battery_left > 100:
+            self.battery_left = 100
+        
+
