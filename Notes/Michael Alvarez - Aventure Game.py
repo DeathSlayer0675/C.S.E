6aa1c567058e46489204f6216e03395e285c19bd
Michael Alvarez <44374124+DeathSlayer0675@users.noreply.github.com>
2019-02-20 09:46:27 -0800

    Bean

--- Notes/Michael Alvarez - Aventure Game.py
+++ Notes/Michael Alvarez - Aventure Game.py
@@ -31,6 +31,42 @@ world_map = {
     },
     "DINING AREA": {
         "NAME": "Dining Area",
+        "DESCRIPTION": "There are a few patry hats on the tables"
+                       "The animatronics ",
+    },
+}
+    "OFFICE": {
+            "NAME": "Security Office",
+        "DESCRIPTION": "This is the room you are in right now."
+                       "There are doors on each side of you that lead "
+                       "to the East and West hallways.",
+        'PATHS': {
+            'WEST': "WEST_HALLWAY",
+            'EAST': "EAST_HALLWAY"
+        }
+    },
+    'WEST_HALLWAY': {
+        "NAME": "West Hallway",
+        "DESCRIPTION": "This hallway connects the dining area to the security office"
+                       "There appears to be a door to your left "
+                       "that leads to the supply closet",
+        "PATHS": {
+            'NORTH': "DINING_AREA",
+            'EAST': "OFFICE",
+            'WEST': "SUPPLY_CLOSET"
+        }
+    },
+    'EAST_HALLWAY': {
+        "NAME": "East Hallway",
+        "DESCRIPTION": "This hallway connects the dining area "
+                       "to the security office",
+        "PATHS": {
+            'NORTH': "DINING_AREA",
+            'WEST': "OFFICE"
+        }
+    },
+    "DINING AREA": {
+        "NAME": "Dining Area",
         "DESCRIPTION": "There are a few ",
     },
 }
