5dc95dd958ececda38b9ea98b9407d6049bc0543
Michael Alvarez <44374124+DeathSlayer0675@users.noreply.github.com>
2019-02-26 09:45:34 -0800

    OOP

--- Notes/World Map OOP.py
+++ Notes/World Map OOP.py
@@ -4,6 +4,27 @@ class Room(object):
         self.north = north
         self.south = south
         self.east = east
+        self.description = description
+
+class Player(object):
+    def __init__(self, starting_location):
+        self.current_location = starting_location
+        self.inventory = []
+
+    def move(self, new_location):
+        """This moves the player to a new location
+
+        :param new_location: The room object of which you are going to
+        """
+        self.current_location = new_location
+
+
+    def find_next_room(self, direction):
+        """This method searches the current
+        :param direction: The direction that you want to move to
+        :return: The Room object if it exists, or None if it doesn't
+        """
+        return getattr (self.current_location, direction)
 
 
 # Option 1 - Define as we go
@@ -15,4 +36,24 @@ R19A.north = parking_lot
 # Option 2 - Set all at once, modify controller
 R19A = Room("Mr. Wiebe's Room", 'parking_lot')
 parking_lot = Room("Parking Lot", None, "R19A")
+
+player = Player(R19A)
+
+playing = True
+directions = ['north', 'south',' east', 'west', 'up', 'down']
+
+while playing:
+    print(player.current_location.name)
+    print(player.current_location.description)
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command.lower() in directions:
+        try:
+            next_room = player.find_next_room(command)
+            player.move(next_room)
+        except KeyError:
+            print("I can't go that way")
+    else:
+        print("Command Not Found")
 
