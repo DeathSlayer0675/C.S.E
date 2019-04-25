64e3f1683ff7bfcdaa9842f7fb48c1a09f8b2f24
Michael Alvarez <44374124+DeathSlayer0675@users.noreply.github.com>
2019-04-25 09:48:02 -0700

    NEW COMPUTER

--- Notes/CSV Notes.py
+++ Notes/CSV Notes.py
@@ -1,7 +1,16 @@
 import csv
 
-with open("Book1.csv") as old_csv:
+with open("Book1.csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
     for row in reader:
-        old_number = int(row[0]) + 1
-            print(old_number)
+        old_number = row[0]
+        # print(int(old_number) + 1)
+        print(old_number)
+
+with open ("MyNewFile.csv", 'w', newline='') as old_csv:
+    print("Writing file...")
+    reader = csv.reader(old_csv)
+    writer = csv.writer(new_csv)
+    for row in reader:
+        old_number = row[0]
+        first_num = int(old_number)
