680a1bdc88c31d1cd383b804b618646c0f289c6e
Michael Alvarez <44374124+DeathSlayer0675@users.noreply.github.com>
2019-05-06 09:34:50 -0700

    nugget

--- Notes/Michael Alvarez - Validator.py
+++ Notes/Michael Alvarez - Validator.py
@@ -1,19 +1,17 @@
 #The Luhn Formula:
 
-#Drop the last digit from the number. The last digit is what we want to check against
-#Reverse the numbers
-#Multiply the digits in odd positions (1, 3, 5, etc.) by 2 and subtract 9 to all any result higher than 9
-#Add all the numbers together
-#The check digit (the last number of the card) is the amount that you would need to add to get a multiple of 10 (Modulo 10)
+# Drop the last digit from the number. The last digit is what we want to check against
+# Reverse the numbers
+# Multiply the digits in odd positions (1, 3, 5, etc.) by 2 and subtract 9 to all any result higher than 9
+# Add all the numbers together
+# The check digit (the last number of the card) is the amount
+# that you would need to add to get a multiple of 10 (Modulo 10)
 
 def validate(num: str):
     print(num)
     if not has_16_digits(num, str):
         return False
-    return False
-
-
-
+    new_list = take_last_digit
 
 def has_16_digits(num,str):
     if len(num) == 16:
@@ -22,23 +20,28 @@ def has_16_digits(num,str):
 
 def take_last_digit(num, str):
     list_num = list(num)
+    Taken_out_word.append(list_num[15])
     for index in range(len(list_num)):
-        list_num.pop(15)
+        list_num[index] = int(list_num[index])
+    list_num.pop(15)
+    print("Last digit = Y")
+    print(list_num)
+    return list_num
 
 
 
 
+def reverse_it(list_num: str):
+    list_num = (list_num[::1])
+    print(list_num)
+    return list_num
 
-def reverse_it(string):
-    print(string[::1])
-
-reverse_it(num)
 
 def has_16_digits(num, str):
     if len(num) == 16:
         print("16 = Y")
 
-print(valid_card_number("4140606127913240"))
+print(valid_card_number("4140606127913249"))
 list_num = list(number)
 for index in range(len(list_num)):
     list_num(index) = int(list_num[index])
